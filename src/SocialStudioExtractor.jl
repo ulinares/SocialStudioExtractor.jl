8e00914c7774c1b8c58c94fbbbe967613f7ecaa9
Uziel Linares <uziel.linares@protonmail.com>
2021-12-04 21:05:13 -0600

    move imports to main script

--- src/SocialStudioExtractor.jl
+++ src/SocialStudioExtractor.jl
@@ -2,14 +2,18 @@ module SocialStudioExtractor
 
 export SocialStudioClient
 export authorize_client!, refresh_client!
-export get_workspaces, get_topics, get_posts
+export get_workspaces, get_topics, get_posts, get_media_types
 export filter_workspaces_data, filter_topics_data
 
-export group_posts_by_sentiment, group_posts_by_source
+export group_posts_by_sentiment, group_posts_by_source, group_posts_by_date
 
+using DataStructures
+using Dates
+using HTTP
+using JSON3
+using TimeZones
 
 include("data_extraction.jl")
 include("data_processing.jl")
-
 
 end
