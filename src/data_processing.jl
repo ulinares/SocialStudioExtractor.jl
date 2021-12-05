f34293fac95437c7a9224ce3675fa4c18f280519
Uziel Linares <uziel.linares@protonmail.com>
2021-12-04 21:09:13 -0600

    improve group by source and add group by date

--- src/data_processing.jl
+++ src/data_processing.jl
@@ -1,14 +1,17 @@
+# Data formatting and processing
+
 function group_posts_by_sentiment(posts_arr::AbstractArray)
     sentiment_mapping = Dict(10 => "positive", -10 => "negative", 0 => "neutral")
     sentiment_dict = Dict(sentiment => [] for sentiment in ["positive", "negative", "neutral"])
 
     for post in posts_arr
+        sentiment = post["sentiment"]
 
-        if length(post["sentiment"]) == 0
+        if length(sentiment) == 0
             continue
         end
 
-        sentiment = sentiment_mapping[sentiment[0]["value"]]
+        sentiment = sentiment_mapping[sentiment[1]["value"]]
         push!(sentiment_dict[sentiment], post)
     end
 
@@ -22,8 +25,24 @@ function group_posts_by_source(posts_arr::AbstractArray)
 
     for post in posts_arr
         source_type = post["media_type"]
-        push!(sources_dict[media_type], post)
+        push!(sources_dict[source_type], post)
     end
 
     return sources_dict
+end
+
+
+function group_posts_by_date(posts_arr::AbstractArray, current_tz::String)
+    date_dict = DefaultDict{String,AbstractArray}(Vector)
+    date_format = DateFormat("Y-m-dTH:M:S\\Z")
+
+    for post in posts_arr
+        date_str = post["publishedDate"]
+        date = ZonedDateTime(DateTime(date_str, date_format), tz"UTC")
+        date = astimezone(date, TimeZone(current_tz))
+        year_month_day = join(yearmonthday(date), "-")
+        push!(date_dict[year_month_day], post)
+    end
+
+    return date_dict
 end
